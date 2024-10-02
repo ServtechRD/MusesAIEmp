3a3dbf514fa9f2e461ff222fb223047c76d5e863
jason <awkwardkao@gmail.com>
2024-10-02 17:11:19 +0800

    fixed ui

--- frontend/src/components/ChatPage.js
+++ frontend/src/components/ChatPage.js
@@ -120,7 +120,7 @@ const InputContainer = styled(Box)(({ theme }) => ({
 const UserName = styled(Typography)(({ theme, isUser }) => ({
   fontSize: 14,
   fontWeight: "bold",
-  color: isUser ? theme.palette.color.info : theme.palette.text.info,
+  color: isUser ? theme.palette.color.primary : theme.palette.text.primary,
   marginBottom: theme.spacing(0.5),
   alignSelf: isUser ? "flex-end" : "flex-start",
 }));
