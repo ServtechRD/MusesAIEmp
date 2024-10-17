e626931aeed2b73745997491e13ced58bcec05d0
jason <awkwardkao@gmail.com>
2024-10-17 13:42:43 +0800

    update code

--- backend/start.sh
+++ backend/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 source ./venv/bin/activate
-uvicorn main:app --host 0.0.0.0 --port 35000 --ssl-keyfile=./cert/privatekey.pem --ssl-certfile=./cert/fullchain.pem
+uvicorn main:app --host 0.0.0.0 --port 35000 --ssl-keyfile=./cert/privatekey.pem --ssl-certfile=./cert/fullchain.pem &
